06f8e37de16b014393a5757129a3049e39135ca0
youngreal <dudwls0505@naver.com>
2026-03-20 15:14:17 +0900

    Initial commit: Spring Boot project setup

---
+++ src/main/java/org/example/makewithjava/MakeWithJavaApplication.java
@@ -0,0 +1,13 @@
+package org.example.makewithjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeWithJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeWithJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/makewithjava/MakeWithJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.makewithjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeWithJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
